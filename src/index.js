665eb174a1e5383677bc4fe74b5a11efd26b696b
Alvigiar <alvigiar.hildrison@gmail.com>
2022-01-09 21:58:45 +0300

    reload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { click } from '@testing-library/user-event/dist/click';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -8,7 +7,20 @@ import ReactDOM from 'react-dom';
 // );
 
 function Click() {
-  console.log('click');
+  fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((post) => {
+      const root = document.querySelector('#root')
+
+      ReactDOM.render(
+        <div>
+          <div>Id: {post.id}</div>
+          <div>Title: {post.title}</div>
+          <div>Body: {post.body}</div>
+        </div>,
+        root
+      )
+    });
 }
 
 const btn = document.querySelector('.btn')
