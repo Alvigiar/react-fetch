0ec34c55f744585ad5d7e3a9b74256159042daf8
Alvigiar Hildrison <80960194+Alvigiar@users.noreply.github.com>
2022-01-13 14:25:50 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-function Click() {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((post) => {
-      const root = document.querySelector('#root')
-      console.log(post);
-      ReactDOM.render(
-        <div>
-          <div>Id: {post.id}</div>
-          <div>Title: {post.title}</div>
-          <div>Body: {post.body}</div>
-        </div>,
-        root
-      )
-    });
+function Clock(props) {
+  return (
+    <div>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
-const btn = document.querySelector('.btn')
-btn.addEventListener('click', () => {
-  Click()
-})
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
